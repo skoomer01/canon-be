81fa055df09ca4ef6651d8d2ad925e01554a98d1
1908a <i476005@fhict.nl>
2023-04-13 16:35:38 +0200

    .

--- BackEnd/CanonBackEnd/src/main/java/codecrusaders/business/IBranchManager.java
+++ BackEnd/CanonBackEnd/src/main/java/codecrusaders/business/IBranchManager.java
@@ -5,6 +5,7 @@ import codecrusaders.domain.Http.RegisterBranchRequest;
 import codecrusaders.domain.Http.RegisterBranchResponse;
 
 public interface IBranchManager {
+    //.
     public RegisterBranchResponse registerBranch(RegisterBranchRequest request);
     public GetAllBranchesResponse getAllBranches();
 
